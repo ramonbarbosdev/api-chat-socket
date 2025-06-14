c1f8d9067db8654730e74dc00598329e43f6d2a4
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-14 11:56:26 -0300

    Adiciona dependência do Springdoc OpenAPI e configurações para documentação da API

--- src/main/java/app/chat/controller/HelloController.java
+++ src/main/java/app/chat/controller/HelloController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController 
 @RequestMapping(value = "/hello")
+@Tag(name = "Teste de funcionamento")
 public class HelloController {
     
-   @GetMapping(value = "/", produces = "application/json")
+    @Operation(description = "Testar comunicação com API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Retorna uma olá mundo")
+    })
+    @GetMapping(value = "/", produces = "application/json")
     public String heloword()
     {
         return "Olá Mundo";
